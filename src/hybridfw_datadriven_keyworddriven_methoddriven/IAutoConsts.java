53fdc142633859c65bbba75281bf140e013f3236
Srikkanth08 <srikanthmetpally2116@gmail.com>
2022-07-30 19:46:25 +0530

    AutomationProject QSpiders

---
+++ src/hybridfw_datadriven_keyworddriven_methoddriven/IAutoConsts.java
@@ -0,0 +1,14 @@
+package hybridfw_datadriven_keyworddriven_methoddriven;
+
+public interface IAutoConsts 
+{
+
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./Drivers/chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./Drivers/geckodriver.exe";
+	
+	String PROP_PATH="./data/config.properties";
+	
+}
